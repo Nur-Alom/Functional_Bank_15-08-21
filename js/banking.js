ee3c989594ddab91d0c87c14ee28ab08245fbe8c
Nur-Alom <alamn7150@gmail.com>
2021-08-16 02:45:56 +0600

    3rd commit

--- js/banking.js
+++ js/banking.js
@@ -1,14 +1,38 @@
+
+
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const amountValue = parseFloat(inputAmountText);
+    // clear input field.
+    inputField.value = '';
+    return amountValue;
+}
+
+function updateTotalField(totalFieldId, Amount) {
+    debugger
+    const totalElement = document.getElementById(totalFieldId);
+    const TotalText = totalElement.innerText;
+    const previousTotal = parseFloat(TotalText);
+    totalElement.innerText = previousTotal + Amount;
+}
+
+
+
 document.getElementById('deposit-btn').addEventListener('click', function () {
-    const depositInput = document.getElementById('deposit');
-    const depositAmountText = depositInput.value;
-    const depositAmount = parseFloat(depositAmountText);
-    // console.log(depositAmount);
+    // const depositInput = document.getElementById('deposit');
+    // const depositAmountText = depositInput.value;
+    // const depositAmount = parseFloat(depositAmountText);
+
+    const depositAmount = getInputValue('deposit');
 
     // get deposit amount
-    const depositTotal = document.getElementById('deposit-amount');
-    const depositTotalText = depositTotal.innerText;
-    const previousDepositTotal = parseFloat(depositTotalText);
-    depositTotal.innerText = previousDepositTotal + depositAmount;
+    // const depositTotal = document.getElementById('deposit-amount');
+    // const depositTotalText = depositTotal.innerText;
+    // const previousDepositTotal = parseFloat(depositTotalText);
+    // depositTotal.innerText = previousDepositTotal + depositAmount;
+
+    updateTotalField('deposit-amount', depositAmount);
 
     // update balance.
     const balanceTotal = document.getElementById('total-balance');
@@ -16,7 +40,42 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     const previousBalanceTotal = parseFloat(balanceTotalText);
     balanceTotal.innerText = previousBalanceTotal + depositAmount;
 
+});
 
-    // clear input field.
-    depositInput.value = '';
-})
+// handel withdraw button.
+
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    // const withdrawInput = document.getElementById('withdraw');
+    // const withdrawAmountText = withdrawInput.value;
+    // const withdrawAmount = parseFloat(withdrawAmountText);
+
+    const withdrawAmount = getInputValue('withdraw');
+
+
+
+    // update withdraw total.
+
+    // const withdrawTotal = document.getElementById('withdraw-amount');
+    // const previousWithdrawTotalText = withdrawTotal.innerText;
+    // const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+    updateTotalField('withdraw-amount', withdrawAmount);
+
+    // withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+
+
+    // update balance after withdraw.
+    const balanceTotal = document.getElementById('total-balance');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+
+
+});
+
+
+
+
+
+
+
+
