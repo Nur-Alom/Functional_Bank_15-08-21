267b8279d19f7a09620893e84140d3f616802fd9
Nur-Alom <alamn7150@gmail.com>
2021-08-16 00:12:38 +0600

    second commit

--- js/banking.js
+++ js/banking.js
@@ -1,14 +1,22 @@
 document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositInput = document.getElementById('deposit');
-    const depositAmount = depositInput.value;
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
     // console.log(depositAmount);
 
     // get deposit amount
     const depositTotal = document.getElementById('deposit-amount');
     const depositTotalText = depositTotal.innerText;
-    depositTotal.innerText = depositAmount;
+    const previousDepositTotal = parseFloat(depositTotalText);
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+
+    // update balance.
+    const balanceTotal = document.getElementById('total-balance');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+
+
     // clear input field.
     depositInput.value = '';
-    console.log(depositTotalText);
-
 })
